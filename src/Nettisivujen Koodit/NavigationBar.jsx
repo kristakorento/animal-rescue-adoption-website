26338fd38c31e1b466c7a162379ace10f293c76e
Krista Korento <krista.korento@gmail.com>
2025-03-21 13:16:23 +0200

    Nagivointi palkki pohja tehty ja routerit liitetty siihen oikeilla nimillä.

--- src/Nettisivujen Koodit/NavigationBar.jsx
+++ src/Nettisivujen Koodit/NavigationBar.jsx
@@ -1,46 +1,89 @@
-import React, { useEffect } from "react";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Link } from "react-router-dom";
 
 function NavigationBar() {
-  useEffect(() => {
-    import("mdb-ui-kit").then((mdb) => {
-      window.mdb = mdb;
-    });
-  }, []);
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <Link className="navbar-brand" to="#">Hope Tails Rescue ry</Link>
         <button
           className="navbar-toggler"
           type="button"
-          data-mdb-target="#navbarNav"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <i className="fas fa-bars"></i>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="adoptionDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Adoption
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="adoptionDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/adoption/info-guides">Info and Guides</Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/adoption/policies">Policies</Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/adoption/fees-and-process">Fees and Process</Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/adoption/faq">FAQ</Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/adoption/trial-adoption">Trial Adoption</Link>
+                </li>
+              </ul>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/features">Features</Link>
+              <Link className="nav-link" to="/dog-profiles">Dog Profiles</Link>
             </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="contanctDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Contact Details
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="contactDropdown">
+                <li>
+                  <Link className="dropdown-item" to="/contact/pre-approval">Pre Approval</Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/contact/privacy-policy">Privacy Policy</Link>
+                </li>
+              </ul>
+              </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/pricing">Pricing</Link>
-            </li>
-            <li className="nav-item">
-              <span className="nav-link disabled">Disabled</span>
+              <Link className="nav-link" to="/blogs">Blog</Link>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default NavigationBar;
