c869658ed2e1c177eb69bccd525387c84856cb6a
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-03-28 16:41:06 +0200

    Navigointipalkkin dropdownit avautuvat hiiren hoverilla.

--- src/Nettisivujen_Koodit/NavigationBar.jsx
+++ src/Nettisivujen_Koodit/NavigationBar.jsx
@@ -1,48 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
+import "../Nettisivujen_Tyylit/NavigationBar.css";
 
 const NavigationBar = () => {
+  const [adoptionOpen, setAdoptionOpen] = useState(false);
+  const [contactOpen, setContactOpen] = useState(false);
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand>Animal Rescue</Navbar.Brand>
+          <Navbar.Brand>Hope Tails</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <LinkContainer to="/">
+            <LinkContainer to="/">
               <Nav.Link>Home</Nav.Link>
             </LinkContainer>
-            <NavDropdown title="Adoption" id="basic-nav-dropdown">
-              <LinkContainer to="/info-guides">
+
+            {/* Adoption Section */}
+            <NavDropdown
+              title={
+                <Link to="/adoption" className="dropdown-title">
+                  Adoption
+                </Link>
+              }
+              id="adoption-dropdown"
+              show={adoptionOpen}
+              onMouseEnter={() => setAdoptionOpen(true)}
+              onMouseLeave={() => setAdoptionOpen(false)}
+            >
+              <LinkContainer to="/adoption/info-guides">
                 <NavDropdown.Item>Info Guides</NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer to="/policies">
+              <LinkContainer to="/adoption/policies">
                 <NavDropdown.Item>Policies</NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer to="/fees-and-process">
+              <LinkContainer to="/adoption/fees-and-process">
                 <NavDropdown.Item>Fees & Process</NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer to="/faq">
+              <LinkContainer to="/adoption/faq">
                 <NavDropdown.Item>FAQ</NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer to="trial-adoption">
+              <LinkContainer to="/adoption/trial-adoption">
                 <NavDropdown.Item>Trial Adoption</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
+
             <LinkContainer to="/dog-profiles">
               <Nav.Link>Dog Profiles</Nav.Link>
             </LinkContainer>
-            <NavDropdown title="Contact Details" id="basic-nav-dropdown">
-              <LinkContainer to="/pre-approval">
+
+            {/* Contact Details Section */}
+            <NavDropdown
+              title={
+                <Link to="/contact-details" className="dropdown-title">
+                  Contact Details
+                </Link>
+              }
+              id="contact-dropdown"
+              show={contactOpen}
+              onMouseEnter={() => setContactOpen(true)}
+              onMouseLeave={() => setContactOpen(false)}
+            >
+              <LinkContainer to="/contact-details/pre-approval">
                 <NavDropdown.Item>Pre-Approval</NavDropdown.Item>
               </LinkContainer>
-              <LinkContainer to="/privacy-policy">
+              <LinkContainer to="/contact-details/privacy-policy">
                 <NavDropdown.Item>Privacy Policy</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
+
             <LinkContainer to="/blogs">
               <Nav.Link>Blogs</Nav.Link>
             </LinkContainer>
