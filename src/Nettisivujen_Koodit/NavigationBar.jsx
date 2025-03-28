fb02bddb7d1876e04efef9796eb9f7795dcfffaf
Krisu07 <krista.korento@gmail.com>
2025-03-28 10:59:47 +0200

    Nettisivujen kansio koodien nimen päivitykset.

--- src/Nettisivujen_Koodit/NavigationBar.jsx
+++ src/Nettisivujen_Koodit/NavigationBar.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Link } from "react-router-dom";
 
 function NavigationBar() {
