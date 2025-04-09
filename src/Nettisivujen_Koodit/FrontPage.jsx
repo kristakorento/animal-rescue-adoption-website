ee945171a316ea15bd909f7df4e57d10a40c0d25
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-04-09 20:38:17 +0300

    Lisätty tyylittelyä paddingeillä yms. ja lisätty responsiivisuutta. Muutettu blogi-postaukset cardeiksi.

--- src/Nettisivujen_Koodit/FrontPage.jsx
+++ src/Nettisivujen_Koodit/FrontPage.jsx
@@ -4,22 +4,24 @@ import '../Nettisivujen_Tyylit/FrontPage.css';
 
 function FrontPage() {
   return (
-    <div className="container">
-      <div className="header text-center">
-        <h1>Hope Tails Rescue ry</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris.</p>
+    <div className="container-fluid p-0">
+      <div className="text-center bg-color p-5">
+        <h1 className="pb-3">Hope Tails Rescue ry</h1>
+        <div className="header-text">
+          <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris.</p>
+        </div>
       </div>
-      <div className="about-container row">
-      <h2 className="text-center">About us</h2>
-        <div className="col">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent condimentum dignissim mi, et consectetur nibh vulputate vel. Mauris id euismod diam. Phasellus vitae ex vehicula, vestibulum lectus quis, venenatis urna. Nunc ac aliquet nulla, non efficitur libero. Aenean consequat, ipsum et efficitur aliquet, nisi ante condimentum mauris, at interdum ante augue quis nisl.</p>
+      <div className="about-container row p-5">
+        <h2 className="text-center">About us</h2>
+        <div className="col-xl-8 col-lg-7 col-12">
+          <p className="pt-4 text-lg-right text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent condimentum dignissim mi, et consectetur nibh vulputate vel. Mauris id euismod diam. Phasellus vitae ex vehicula, vestibulum lectus quis, venenatis urna. Nunc ac aliquet nulla, non efficitur libero. Aenean consequat, ipsum et efficitur aliquet, nisi ante condimentum mauris, at interdum ante augue quis nisl.</p>
         </div>
         <div className="col col-lg-4">
           <img src={img1} alt="testikuva src:https://pixabay.com/fi/photos/lemmikkiel%C3%A4imet-koira-samojedi-1356191/" className="homepage-img" />
         </div>
       </div>
-      <div className="blogs-container text-center row justify-content-around">
-        <h2>Check these blog posts out:</h2>
+      <div className="blogs-container text-center row justify-content-around bg-color p-5">
+        <h2 className="pb-5">Check these blog posts out:</h2>
         <BlogPost />
         <BlogPost />
         <BlogPost />
@@ -30,9 +32,11 @@ function FrontPage() {
 
 function BlogPost() {
   return (
-    <div className="w-25 border">
-      <img src={img2} alt="testikuva src:https://pixabay.com/fi/photos/koira-corgi-s%C3%B6p%C3%B6-el%C3%A4in-4988986/" className="homepage-img2"/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. </p>
+    <div className="card" style={{ width: '20rem' }}>
+      <img className="card-img-top" src={img2} alt="testikuva src:https://pixabay.com/fi/photos/koira-corgi-s%C3%B6p%C3%B6-el%C3%A4in-4988986/" />
+      <div class="card-body">
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. </p>
+      </div>
     </div>
   )
 }
