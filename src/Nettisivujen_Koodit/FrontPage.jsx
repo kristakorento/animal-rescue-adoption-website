0d976cc26a44690834ceaf1eff050eeafb049447
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-04-11 19:52:38 +0300

    Responsiivisuutta lisätty kuvaan ja blogi-postauksiin, sekä pienempiä korjauksia tehty.

--- src/Nettisivujen_Koodit/FrontPage.jsx
+++ src/Nettisivujen_Koodit/FrontPage.jsx
@@ -14,28 +14,32 @@ function FrontPage() {
       <div className="about-container row p-5">
         <h2 className="text-center">About us</h2>
         <div className="col-xl-8 col-lg-7 col-12">
-          <p className="pt-4 text-lg-right text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent condimentum dignissim mi, et consectetur nibh vulputate vel. Mauris id euismod diam. Phasellus vitae ex vehicula, vestibulum lectus quis, venenatis urna. Nunc ac aliquet nulla, non efficitur libero. Aenean consequat, ipsum et efficitur aliquet, nisi ante condimentum mauris, at interdum ante augue quis nisl.</p>
+          <p className="pt-3 text-lg-end text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent condimentum dignissim mi, et consectetur nibh vulputate vel. Mauris id euismod diam. Phasellus vitae ex vehicula, vestibulum lectus quis, venenatis urna. Nunc ac aliquet nulla, non efficitur libero. Aenean consequat, ipsum et efficitur aliquet, nisi ante condimentum mauris, at interdum ante augue quis nisl.</p>
         </div>
-        <div className="col col-lg-4">
-          <img src={img1} alt="testikuva src:https://pixabay.com/fi/photos/lemmikkiel%C3%A4imet-koira-samojedi-1356191/" className="homepage-img" />
+        <div className="col col-lg-4 flex-between-600-991">
+          <img src={img1} alt="testikuva src:https://pixabay.com/fi/photos/lemmikkiel%C3%A4imet-koira-samojedi-1356191/" className="homepage-img pt-4" />
         </div>
       </div>
-      <div className="blogs-container text-center row justify-content-around bg-color p-5">
+      <div className="blogs-container text-center justify-content-around bg-color p-5">
         <h2 className="pb-5">Check these blog posts out:</h2>
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
+        <div className="d-flex flex-wrap justify-content-center gap-4">
+          <BlogPost />
+          <BlogPost hideOnMd/>
+          <BlogPost hideOnLg/>
+        </div>
       </div>
     </div>
   )
 }
 
-function BlogPost() {
+function BlogPost({ hideOnLg = false ,hideOnMd = false}) {
   return (
-    <div className="card" style={{ width: '20rem' }}>
-      <img className="card-img-top" src={img2} alt="testikuva src:https://pixabay.com/fi/photos/koira-corgi-s%C3%B6p%C3%B6-el%C3%A4in-4988986/" />
-      <div class="card-body">
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. </p>
+    <div className={`mb-4 d-flex justify-content-center ${hideOnLg ? 'd-none d-xl-flex' : ''} ${hideOnMd ? 'd-none d-md-flex' : ''}`}>
+      <div className="card h-100" style={{ width: '20rem' }}>
+        <img className="card-img-top" src={img2} alt="testikuva src:https://pixabay.com/fi/photos/koira-corgi-s%C3%B6p%C3%B6-el%C3%A4in-4988986/" />
+        <div class="card-body">
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. </p>
+        </div>
       </div>
     </div>
   )
