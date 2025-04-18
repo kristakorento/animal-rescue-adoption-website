00f2438a6b423ba11f98ca103760cc59b37ecfec
Krista Korento <krista.korento@gmail.com>
2025-04-18 11:06:35 +0300

    DogProfiles sivun layout koodi tehty.

--- src/Nettisivujen_Koodit/DogProfiles.jsx
+++ src/Nettisivujen_Koodit/DogProfiles.jsx
@@ -1,80 +1,101 @@
-import React from "react";
-import testi1 from "../assets/images/testi1.jpg";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import testi1 from '../assets/images/testi1.jpg'
 
 function DogProfiles() {
   return (
-    <div className="min-h-screen bg-white">
-
-      {/* Header */}
-      <div className="text-center py-10">
-        <h1 className="text-4xl font-bold">Dogs</h1>
-        <p className="mt-4 text-gray-500 max-w-xl mx-auto">
-          Find your new best friend from our adorable pups waiting for a forever home.
-        </p>
-      </div>
-
-      {/* White Box Container for Dog Cards */}
-      <div className="bg-white px-4 py-8">
-        {/* Grid Layout with 3 cards per row */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-
-          {/* Dog Card 1 */}
-          <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col items-center transition hover:scale-105 duration-300">
-            <img src={testi1} alt="Dog 1" className="w-full h-48 rounded-xl mb-4 object-cover" />
-            <h2 className="text-2xl font-semibold text-gray-800">Bella</h2>
-            <p className="text-gray-600">Breed: Golden Retriever</p>
-            <p className="text-gray-600">Age: 2 years</p>
-            <p className="text-gray-600">Location: Woofville</p>
-          </div>
-
-          {/* Dog Card 2 */}
-          <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col items-center transition hover:scale-105 duration-300">
-            <img src={testi1} alt="Dog 2" className="w-full h-48 rounded-xl mb-4 object-cover" />
-            <h2 className="text-2xl font-semibold text-gray-800">Max</h2>
-            <p className="text-gray-600">Breed: Beagle</p>
-            <p className="text-gray-600">Age: 3 years</p>
-            <p className="text-gray-600">Location: Barktown</p>
-          </div>
-
-          {/* Dog Card 3 */}
-          <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col items-center transition hover:scale-105 duration-300">
-            <img src={testi1} alt="Dog 3" className="w-full h-48 rounded-xl mb-4 object-cover" />
-            <h2 className="text-2xl font-semibold text-gray-800">Luna</h2>
-            <p className="text-gray-600">Breed: Husky</p>
-            <p className="text-gray-600">Age: 1 year</p>
-            <p className="text-gray-600">Location: Snuggle City</p>
-          </div>
-
-          {/* Dog Card 4 */}
-          <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col items-center transition hover:scale-105 duration-300">
-            <img src={testi1} alt="Dog 4" className="w-full h-48 rounded-xl mb-4 object-cover" />
-            <h2 className="text-2xl font-semibold text-gray-800">Charlie</h2>
-            <p className="text-gray-600">Breed: Poodle</p>
-            <p className="text-gray-600">Age: 4 years</p>
-            <p className="text-gray-600">Location: Pawradise</p>
-          </div>
-
-          {/* Dog Card 5 */}
-          <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col items-center transition hover:scale-105 duration-300">
-            <img src={testi1} alt="Dog 5" className="w-full h-48 rounded-xl mb-4 object-cover" />
-            <h2 className="text-2xl font-semibold text-gray-800">Daisy</h2>
-            <p className="text-gray-600">Breed: Labrador</p>
-            <p className="text-gray-600">Age: 2.5 years</p>
-            <p className="text-gray-600">Location: Tailtown</p>
-          </div>
-
-          {/* Dog Card 6 */}
-          <div className="bg-white rounded-2xl shadow-lg p-6 flex flex-col items-center transition hover:scale-105 duration-300">
-            <img src={testi1} alt="Dog 6" className="w-full h-48 rounded-xl mb-4 object-cover" />
-            <h2 className="text-2xl font-semibold text-gray-800">Rocky</h2>
-            <p className="text-gray-600">Breed: Border Collie</p>
-            <p className="text-gray-600">Age: 5 years</p>
-            <p className="text-gray-600">Location: Floofy Hills</p>
-          </div>
-
-        </div>
-      </div>
-    </div>
+    <Container className="gx-5 gy-5">
+      <Row>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 2" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 3" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 3" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 3" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 3" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 3" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
+          <img src={testi1} alt="Kuva 3" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+          <h5 className="mt-3">Nimi: Bella</h5>
+          <p>Rotu: Labradorinnoutaja</p>
+          <p>Ikä: 2 vuotta</p>
+          <p>Luonne: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+          <p>Muuta: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
