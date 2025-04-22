dfa61393e1524c864aa0c4d09a8273e274342658
Krista Korento <krista.korento@gmail.com>
2025-04-22 12:12:52 +0300

    Privacy Policy ja Trial Adoption layouteista poistettu koodista text muted osiot.

--- src/Nettisivujen_Koodit/PrivacyPolicy.jsx
+++ src/Nettisivujen_Koodit/PrivacyPolicy.jsx
@@ -13,22 +13,22 @@ function PrivacyPolicy(){
         <Row className="justify-content-center"  style={{ margintop:"4rem" ,marginBottom: "4rem"}}>
           <Col md={8} lg={6} className="text-center">
             <h5>What Information We Collect</h5>
-            <p className="text-muted"> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+            <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
 
             <h5>Why We Collect Your Data</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>How We Store and Protect Your Data</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>How Long We Keep Your Data</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>Cookies and Website Analytics</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>Your Rights Under GDPR</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </Col>
         </Row>
       </Container>
--- src/Nettisivujen_Koodit/TrialAdoption.jsx
+++ src/Nettisivujen_Koodit/TrialAdoption.jsx
@@ -13,22 +13,22 @@ function TrialAdoption() {
         <Row className="justify-content-center"  style={{ margintop:"4rem" ,marginBottom: "4rem"}}>
           <Col md={8} lg={6} className="text-center">
             <h5>What Is a Trial Adoption?</h5>
-            <p className="text-muted"> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+            <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
 
             <h5>How Long Does the Trial Last?</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>What's Required from the Adopter?</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>Is There a Fee?</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>After the Trial</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
             <h5>Important Notes</h5>
-            <p className="text-muted" style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p style={{ lineHeight: "2.0"}}> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </Col>
         </Row>
       </Container>
