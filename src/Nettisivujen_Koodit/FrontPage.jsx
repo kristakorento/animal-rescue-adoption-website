bd6b4e8341593d30cd45b8fa18f881c28a077a75
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-04-23 19:49:05 +0300

    Etusivulle lisätty teksti sisältö ja kuva.

--- src/Nettisivujen_Koodit/FrontPage.jsx
+++ src/Nettisivujen_Koodit/FrontPage.jsx
@@ -1,5 +1,4 @@
-import img1 from "../assets/images/pets-1356191_1280.jpg";
-import img2 from "../assets/images/dog-4988986_1280.jpg";
+import frontpageimg from "../assets/images/about-us.jpg";
 import { BlogPost } from "./Blogs";
 import '../Nettisivujen_Tyylit/FrontPage.css';
 
@@ -9,24 +8,32 @@ function FrontPage() {
       <div className="text-center bg-color-yellow p-5">
         <h1 className="pb-3">Hope Tails Rescue ry</h1>
         <div className="header-text">
-          <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris.</p>
+          <p>At Hope Tails Rescue, we’re on a mission to give every animal the love, care, and second chance they deserve. We are a dedicated, volunteer-driven animal shelter focused on rescuing abandoned, neglected, and homeless dogs — and helping them find the forever families they’ve been waiting for. </p>
         </div>
       </div>
-      <div className="about-container bg-color-gray row p-5 mr-0">
-        <h2 className="text-center">About us</h2>
-        <div className="col-xl-8 col-lg-7 col-12">
-          <p className="pt-3 text-lg-end text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum elementum varius. Nullam eu ligula sed diam placerat auctor. Nam mauris arcu, placerat sed quam non, interdum vulputate metus. In eget justo accumsan, vestibulum risus at, ultricies sem. Cras ante orci, vulputate eu iaculis et, feugiat non turpis. Mauris rutrum, mi et pharetra posuere, lectus felis molestie neque, non posuere purus diam vitae orci. Integer imperdiet metus tortor, sed luctus quam fermentum eget. Nullam at lectus ex. Donec consequat lectus mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent condimentum dignissim mi, et consectetur nibh vulputate vel. Mauris id euismod diam. Phasellus vitae ex vehicula, vestibulum lectus quis, venenatis urna. Nunc ac aliquet nulla, non efficitur libero. Aenean consequat, ipsum et efficitur aliquet, nisi ante condimentum mauris, at interdum ante augue quis nisl.</p>
+      <div className="about-container bg-color-gray row p-5 m-0 text-center">
+        <h2 className="">About us</h2>
+        <div className="col-xl-8 col-lg-7 col-12 text-lg-end">
+          <p className="pt-3">Based on compassion and community, Hope Tails is more than just a shelter — it’s a safe haven. Every dog that comes through our doors receives medical attention, nutritious food, and most importantly, unconditional love. Whether they’ve been saved from the streets of Spain, surrendered by owners, or rescued from abusive situations, we welcome them with open arms and a promise of a better future.</p>
+
+
+
+            <p>We believe that every tail deserves a happy ending. Through adoption, education, and advocacy, we work tirelessly to reduce the number of animals in need and to build a more compassionate world for our furry friends.</p>
+
+
+
+            <p>Ready to be part of the change? Whether you're looking to adopt, foster, volunteer, or donate — there's a place for you in the Hope Tails family.</p>
         </div>
         <div className="col col-lg-4 flex-between-600-991">
-          <img src={img1} alt="testikuva src:https://pixabay.com/fi/photos/lemmikkiel%C3%A4imet-koira-samojedi-1356191/" className="homepage-img pt-4" />
+          <img src={frontpageimg} alt="Golden retriver and a woman high-fiving" className="homepage-img pt-4" />
         </div>
       </div>
       <div className="blogs-container text-center justify-content-around bg-color-yellow p-5">
         <h2 className="pb-5">Check these blog posts out:</h2>
         <div className="d-flex flex-wrap justify-content-center gap-4">
           <BlogPost />
-          <BlogPost hideOnMd/>
-          <BlogPost hideOnLg/>
+          <BlogPost hideOnMd />
+          <BlogPost hideOnLg />
         </div>
       </div>
     </div>
