265108f8af77ee528910e7d9aebf6fbab08ad4fc
Krista Korento <krista.korento@gmail.com>
2025-04-24 09:17:24 +0300

    DogProfiles sivun ongelmia korjailtu.

--- src/Nettisivujen_Koodit/DogProfiles.jsx
+++ src/Nettisivujen_Koodit/DogProfiles.jsx
@@ -1,96 +1,105 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import testi1 from '../assets/images/testi1.jpg'
+import testi1 from '../assets/images/testi1.jpg';
+import '../Nettisivujen_Tyylit/DogProfiles.css';
 
 function DogProfiles() {
-  return (
-    <Container className="gx-5 gy-5">
-       <h2 className="text-center mb-4">Dogs</h2>
-        <p className="text-center mb-5">
-          Tutustu ihanaihin koirakamuihimme! Jokaisella on oma uniikki luonne ja tarina. 
-          Ehkä löydät uuden suosikin tai oman bestiksen!
-        </p>
-      <Row>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-        <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
-          <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
-          <h5 className="mt-3">Name: Bella</h5>
-          <p>Dog breed:: Labradorinnoutaja</p>
-          <p>Age: 2 vuotta</p>
-          <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
-          <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </Col>
-      </Row>
-    </Container>
-  );
+  return ( 
+          <Container>
+            <div fluid className="bg-color-yellow py-4">
+              <Container className="gx-5 gy-5">
+                <h2 className="text-center mb-4">Dogs</h2>
+                <p className="text-center mb-5">
+                Tutustu ihanaihin koirakamuihimme! Jokaisella on oma uniikki luonne ja tarina. 
+                Ehkä löydät uuden suosikin tai oman bestiksen!
+                </p>
+              </Container>
+            </div>
+            <div className='bg-color-grey'>
+              <Container>
+                <Row className="justify-content-center gx-4 gy-4">
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <img src={testi1} alt="Kuva 1" style={{ width: '300px', height: 'auto' }}  className="img-fluid rounded shadow" />
+                    <h5 className="mt-3">Name: Bella</h5>
+                    <p>Dog breed:: Labradorinnoutaja</p>
+                    <p>Age: 2 vuotta</p>
+                    <p>Personality: Ystävällinen, leikkisä ja rakastaa herkkuja! </p>
+                    <p>Other information: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+                    but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  </Col>
+                </Row>
+                </Container>
+            </div>
+        </Container>
+  )
 }
 
 export default DogProfiles;
