65f8d708d24b9ad43ad4d61cfab4f804be7a2e73
miia-mariastenius <n3stmi00@studens.oamk.fi>
2025-04-27 18:42:00 +0300

    Footeriin lisätty logo ja korjauksia responsiivisuuteen. Navbariin lisätty logo ja burgermenuun korjauksia.

--- src/Nettisivujen_Koodit/NavigationBar.jsx
+++ src/Nettisivujen_Koodit/NavigationBar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/images/logo-ympyrä.png"
 import "../Nettisivujen_Tyylit/NavigationBar.css";
 
 const NavigationBar = () => {
@@ -10,6 +11,7 @@ const NavigationBar = () => {
   const [expanded, setExpanded] = useState(false);
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 992);
 
+  //Change hover effect on dropdowns based on screen size
   useEffect(() => {
     const handleResize = () => {
       setIsLargeScreen(window.innerWidth >= 992);
@@ -22,18 +24,47 @@ const NavigationBar = () => {
     setExpanded(false);
   };
 
+  //Close dropdowns when clicking outside
+  const handleDropdownClick = (dropdown) => {
+    if (dropdown === "adoption") {
+      setAdoptionOpen(!adoptionOpen);
+      setContactOpen(false);
+    } else if (dropdown === "contact") {
+      setContactOpen(!contactOpen);
+      setAdoptionOpen(false);
+    }
+  };
+
+  const handleTitleClick = () => {
+    setExpanded(false); // Close the burger menu when a title is clicked
+    setAdoptionOpen(false); // Close all dropdowns
+    setContactOpen(false);
+  };
+
+  const handlePageClick = () => {
+    setExpanded(false); // Close the burger menu when a page is clicked
+    setAdoptionOpen(false); // Close all dropdowns
+    setContactOpen(false);
+  };
+
   return (
     <Navbar expand="lg" className="nav-bg-color py-1" expanded={expanded}>
       <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>Hope Tails</Navbar.Brand>
-        </LinkContainer>
+        <Navbar.Brand>
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Hope Tails Logo"
+              style={{ height: "40px" }}
+            />
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
           onClick={() => setExpanded(!expanded)}
         />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto" onSelect={handleNavSelect}>
+          <Nav className="ms-auto" onSelect={handlePageClick} activeKey="/">
             <LinkContainer to="/">
               <Nav.Link className="nav-title">Home</Nav.Link>
             </LinkContainer>
@@ -42,14 +73,18 @@ const NavigationBar = () => {
             <NavDropdown
               title={
                 <>
-                  <Link to="/adoption" className="dropdown-title">
+                  <Link
+                    to="/adoption"
+                    className="dropdown-title"
+                    onClick={handleTitleClick}
+                  >
                     Adoption
                   </Link>
                   <Button
                     className="dropdown-toggle-button"
                     onClick={(e) => {
                       e.preventDefault()
-                      setAdoptionOpen(!adoptionOpen)
+                      handleDropdownClick("adoption")
                     }}
                   >
                     ▼
@@ -62,48 +97,54 @@ const NavigationBar = () => {
               onMouseLeave={() => isLargeScreen && setAdoptionOpen(false)}
             >
               <LinkContainer to="/adoption/info-guides">
-                <NavDropdown.Item onClick={() => setAdoptionOpen(false)}>
+                <NavDropdown.Item onClick={handlePageClick}>
                   Info & Guides
                 </NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/adoption/policies">
-                <NavDropdown.Item onClick={() => setAdoptionOpen(false)}>
+                <NavDropdown.Item onClick={handlePageClick}>
                   Policies
                 </NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/adoption/fees-and-process">
-                <NavDropdown.Item onClick={() => setAdoptionOpen(false)}>
+                <NavDropdown.Item onClick={handlePageClick}>
                   Fees & Process
                 </NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/adoption/faq">
-                <NavDropdown.Item onClick={() => setAdoptionOpen(false)}>
+                <NavDropdown.Item onClick={handlePageClick}>
                   FAQ
                 </NavDropdown.Item>
               </LinkContainer>
               <LinkContainer to="/adoption/trial-adoption">
-                <NavDropdown.Item onClick={() => setAdoptionOpen(false)}>
+                <NavDropdown.Item onClick={handlePageClick}>
                   Trial Adoption
                 </NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
 
             <LinkContainer to="/dog-profiles">
-              <Nav.Link className="nav-title">Dogs</Nav.Link>
+              <Nav.Link className="nav-title" onClick={handlePageClick}>
+                Dogs
+              </Nav.Link>
             </LinkContainer>
 
             {/* Contact Details Section */}
             <NavDropdown
               title={
                 <>
-                  <Link to="/contact-details" className="dropdown-title">
+                  <Link
+                    to="/contact-details"
+                    className="dropdown-title"
+                    onClick={handleTitleClick}
+                  >
                     Contact Details
                   </Link>
                   <Button
                     className="dropdown-toggle-button"
                     onClick={(e) => {
                       e.preventDefault()
-                      setContactOpen(!contactOpen);
+                      handleDropdownClick("contact");
                     }}
                   >
                     ▼
@@ -116,14 +157,14 @@ const NavigationBar = () => {
               onMouseLeave={() => isLargeScreen && setContactOpen(false)}
             >
               <LinkContainer to="/contact-details/privacy-policy">
-                <NavDropdown.Item onClick={() => setContactOpen(false)}>
+                <NavDropdown.Item onClick={handlePageClick}>
                   Privacy Policy
                 </NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
 
             <LinkContainer to="/blogs">
-              <Nav.Link className="nav-title">Blog</Nav.Link>
+              <Nav.Link className="nav-title" onClick={handlePageClick}>Blog</Nav.Link>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
